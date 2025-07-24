deaa0debd0e36692cca5893691d3ee5bb55e766f
Łukasz Kosson <git@lukasz.kosson.net>
2025-07-24 17:29:41 +0200

    Removed "ago" from relative time specifier

--- Pages/Components/RelTimeViewComponent.cs
+++ Pages/Components/RelTimeViewComponent.cs
@@ -13,10 +13,10 @@ public class RelTimeViewComponent : ViewComponent
 	{
 		if (timestamp == null) return "";
 		var diff = DateTime.Now - timestamp.Value;
-		if (diff.TotalSeconds < 1) return "now";
-		if (diff.TotalSeconds < 60) return $"{diff.TotalSeconds:0}s ago";
-		if (diff.TotalMinutes < 60) return $"{Math.Floor(diff.TotalMinutes):0}min {diff.Seconds:0}s ago";
-		if (diff.TotalHours < 24) return $"{Math.Floor(diff.TotalHours):0}h {diff.Minutes}min ago";
+		if (diff.TotalSeconds < 1) return "just now";
+		if (diff.TotalSeconds < 60) return $"{diff.TotalSeconds:0}s";
+		if (diff.TotalMinutes < 60) return $"{Math.Floor(diff.TotalMinutes):0}min {diff.Seconds:0}s";
+		if (diff.TotalHours < 24) return $"{Math.Floor(diff.TotalHours):0}h {diff.Minutes}min";
 		if (timestamp.Value.Date == DateTime.Now.Date.AddDays(-1)) return $"yesterday {timestamp:HH:mm}";
 		return timestamp.Value.ToString("yyyy-MM-dd HH:mm:ss");
 	}
