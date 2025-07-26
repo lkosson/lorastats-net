14a1f68a57d717eb90bde3aa2c6e7e1296c777ab
Łukasz Kosson <git@lukasz.kosson.net>
2025-07-26 18:52:54 +0200

    Optimized queries for Node page

--- Pages/Node.cshtml.cs
+++ Pages/Node.cshtml.cs
@@ -16,7 +16,7 @@ class NodeModel(LoraStatsNetDb db) : PageModel, IPageWithTitle
 	public Node Node { get; set; } = default!;
 	public List<Packet> PacketsSent { get; set; } = default!;
 	public List<Packet> PacketsReceived { get; set; } = default!;
-	public List<(Node node, double? distance, int[] hopStats, float hopAvg, PacketReport lastReport, int packetPct)> NodesReached { get; set; } = default!;
+	public List<(Node node, double? distance, int[] hopStats, float hopAvg, EntityRef<PacketReport> lastReportRef, DateTime lastReportTime, int packetPct)> NodesReached { get; set; } = default!;
 	public List<(Node node, double? distance, byte rssi, float snr)> NodesReachedDirectly { get; set; } = default!;
 
 	public async Task<IActionResult> OnGetAsync()
@@ -24,26 +24,31 @@ class NodeModel(LoraStatsNetDb db) : PageModel, IPageWithTitle
 		if (NodeRef.IsNull) return BadRequest();
 		var node = await db.Nodes.Include(node => node.Community).FirstOrDefaultAsync(node => node.Ref == NodeRef);
 		if (node == null) return NotFound();
-		var reports = await db.PacketReports
-			.Include(report => report.Packet)
-			.ThenInclude(report => report.FromNode)
-			.Include(report => report.Packet)
-			.ThenInclude(report => report.ToNode)
-			.Include(report => report.Gateway)
-			.Where(report => report.Packet.FromNodeId == NodeRef || report.Packet.ToNodeId == NodeRef)
+		var nodes = await db.Nodes.Where(node => node.CommunityId == node.CommunityId).ToListAsync();
+
+		var packets = await db.Packets
+			.Include(packet => packet.FromNode)
+			.Include(packet => packet.ToNode)
+			.Where(packet => packet.FromNodeId == NodeRef || packet.ToNodeId == NodeRef)
 			.ToListAsync();
-		var packets = reports.Select(report => report.Packet).Distinct().ToList();
+
+		var reports = await db.PacketReports
+			.Where(report => report.Packet.FromNodeId == NodeRef)
+			.Select(report => new { report.Ref, report.GatewayId, report.HopLimit, report.ReceptionTime, report.PacketId, report.RSSI, report.SNR, report.Packet.HopStart })
+			.ToListAsync();
+
+		var nodesByRef = nodes.ToDictionary(node => node.Ref);
 
 		RouteData.Values["community"] = node.Community?.UrlName;
 
-		(int[] pct, float avg) HopStats(IEnumerable<PacketReport> reports)
+		(int[] pct, float avg) HopStats(IEnumerable<(byte hopStart, byte hopLimit)> reports)
 		{
 			int[] hops = new int[8];
 			int count = 0;
 			var sum = 0;
-			foreach (var report in reports)
+			foreach (var (hopStart, hopLimit) in reports)
 			{
-				int hopsAway = report.Packet.HopStart - report.HopLimit;
+				int hopsAway = hopStart - hopLimit;
 				if (hopsAway < 0) hopsAway = 0;
 				if (hopsAway > 7) hopsAway = 7;
 				hops[hopsAway]++;
@@ -67,11 +72,10 @@ class NodeModel(LoraStatsNetDb db) : PageModel, IPageWithTitle
 			.ToList();
 
 		NodesReached = reports
-			.Where(report => report.Packet.FromNodeId == node)
-			.GroupBy(report => report.Gateway)
+			.GroupBy(report => report.GatewayId)
 			.Select(gateway => (
-				gateway: gateway.Key,
-				hopStats: HopStats(gateway),
+				gateway: nodesByRef.GetValueOrDefault(gateway.Key)!,
+				hopStats: HopStats(gateway.Select(report => (report.HopStart, report.HopLimit))),
 				lastReport: gateway.OrderBy(report => report.ReceptionTime).Last(),
 				packetCount: gateway.Select(report => report.PacketId).Distinct().Count()))
 			.Distinct()
@@ -81,7 +85,8 @@ class NodeModel(LoraStatsNetDb db) : PageModel, IPageWithTitle
 				distance: e.gateway!.DistanceTo(node),
 				hopStats: e.hopStats.pct,
 				hopAvg: e.hopStats.avg,
-				e.lastReport,
+				lastReportRef: e.lastReport.Ref,
+				lastReportTime: e.lastReport.ReceptionTime,
 				packetPct: 100 * e.packetCount / PacketsSent.Count))
 			.OrderByDescending(e => e.distance)
 			.ThenByDescending(e => e.packetPct)
@@ -89,10 +94,9 @@ class NodeModel(LoraStatsNetDb db) : PageModel, IPageWithTitle
 			.ToList();
 
 		NodesReachedDirectly = reports
-			.Where(report => report.Packet.FromNodeId == node)
-			.Where(report => report.HopLimit == report.Packet.HopStart)
-			.GroupBy(report => report.Gateway)
-			.Select(gateway => (gateway: gateway.Key, reports: gateway))
+			.Where(report => report.HopLimit == report.HopStart)
+			.GroupBy(report => report.GatewayId)
+			.Select(gateway => (gateway: nodesByRef.GetValueOrDefault(gateway.Key)!, reports: gateway))
 			.Where(e => e.gateway != node)
 			.Select(e => (
 				node: e.gateway,
